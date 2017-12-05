0cfce231832fd3cae916d51f95ec61ad70dff6fb
hecter_wu <hecter_wu@transcend.com.tw>
2017-12-05 14:46:27 +0800

    pass testcase: testOnlyOneNumber by using dictionary

--- Letter Combinations of a Phone Number.py
+++ Letter Combinations of a Phone Number.py
@@ -5,12 +5,31 @@ class unitest(unittest.TestCase):
         Input = ""
         Output = []
         self.assertEqual(Solution().letterCombinations(Input),Output);
+    def testOnlyOneNumber(self):
+        Input = "2"
+        Output = ["a","b","c"]
+        self.assertEqual(Solution().letterCombinations(Input),Output);
 
 class Solution():
     def letterCombinations(self, digits):
         if digits == "":
             return []
-        return None
+        dic = {"1" : ["*"],
+               "2" : ["a","b","c"],
+               "3" : ["d","e","f"],
+               "4" : ["g","h","i"],
+               "5" : ["j","k","l"],
+               "6" : ["m","n","o"],
+               "7" : ["p","q","r","s"],
+               "8" : ["t","u","v"],
+               "9" : ["w","x","y","z"],
+               "0" : [" "]}
+        ans = []
+        for item in digits:
+            for dicitem in dic[item]:
+                ans.append(dicitem)
+
+        return ans
 
 if __name__ == '__main__':
     unittest.main()
